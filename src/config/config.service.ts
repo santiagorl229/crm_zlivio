f01c9fd64e1cd2ad68cd99bc140b89f566661bbc
Tu Nombre <santiagorl229@gmail.com>
2021-05-19 18:24:13 -0500

    Cambio en el main.ts origin y optionSuccesStatus

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,35 +1,23 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { MODE, POSTGRES_HOST, POSTGRES_PORT } from 'src/constants';
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+import { POSTGRES_DATABASE, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER } from 'src/constants';
+require('dotenv').config();
 
 class ConfigService {
-
-  constructor() { }
-
-  public isProduction() {
-    return MODE != 'DEV';
-  }
-
-  public getTypeOrmConfig(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: POSTGRES_HOST,
-      port: POSTGRES_PORT,
-      username: this.getValue('POSTGRES_USER'),
-      password: this.getValue('POSTGRES_PASSWORD'),
-      database: this.getValue('POSTGRES_DATABASE'),
-      entities: ['**/*.entity{.ts,.js}'],
-      migrationsTableName: 'migration',
-      migrations: ['src/migration/*.ts'],
-      cli: {
-        migrationsDir: 'src/migration',
-      },
-      ssl: this.isProduction(),
-    };
-  }
-    getValue(arg0: string): string {
-        throw new Error('Method not implemented.');
+    constructor() {}
+    
+    public getTypeOrmConfig(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            keepConnectionAlive: true,
+            host: POSTGRES_HOST,
+            port: POSTGRES_PORT || 3306,
+            database: POSTGRES_DATABASE,
+            username: POSTGRES_USER,
+            password: POSTGRES_PASSWORD,
+            entities: [__dirname + '/../**/*.entities{.ts,.js}']
+        };
     }
 }
 
-const configService = new ConfigService()
-export { configService };
+const configService = new ConfigService();
+export {configService};
--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ dotenv.config();
 export const POSTGRES_HOST= '127.0.0.1';
 export const POSTGRES_PORT=5432
 export const POSTGRES_USER='postgres'
-export const POSTGRES_PASSWORD='Zlivio*1*C_2_R.3.M'
-export const POSTGRES_DATABASE='crm_zlivio'
+export const POSTGRES_PASSWORD='postgres'
+export const POSTGRES_DATABASE='crmZlivio'
 export const PORT=3000
 export const MODE='DEV'
 export const RUN_MIGRATIONS=true
