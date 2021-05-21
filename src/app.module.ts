f3fb25938a4a5191feffc9d3e0940141a10f6213
Tu Nombre <santiagorl229@gmail.com>
2021-05-21 16:34:23 -0500

    Avance en la configuracion del proyecto CRM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
+import { ItemController } from './item/item.controller';
+import { ItemService } from './item/item.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ItemController],
+  providers: [AppService, ItemService],
 })
 export class AppModule {}
